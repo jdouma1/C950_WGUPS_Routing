4d98003cef952febe1575c28e82c639e76fc6714
Jacob Douma <doumajacob24@gmail.com>
2024-02-18 17:07:29 -0800

    Defines insert() method for HashTable and adds comments for this method and prior class code.

--- HashTable.py
+++ HashTable.py
@@ -1,7 +1,22 @@
+# Class used for storing and manipulating packages in a hash table using chaining
 class HashTable:
-    def __init__(self, initialCapacity = 40):
-        self.table = []
 
+    # Constructor for hash table with default initial capacity set to 40
+    # The default capacity refers to the number of packages to be delivered
+    # Each bucket is initially assigned an empty list
+    def __init__(self, initialCapacity = 40):
+        # Begin initializing hash table with empty bucket list entries
+        self.table = []
         for i in range(initialCapacity):
             self.table.append([])
 
+    #
+    def insert(self, item):
+        # The item's bucket is the result of first hashing the data into numerical form
+        # and then performing 'the resulting hash' mod table length (in this case 40)
+        bucket = hash(item) % len(self.table)
+
+        # The prior resulting bucket is used to pull the bucket list from the table
+        # The item is then appended to the bucket's list
+        bucketList = self.table[bucket]
+        bucketList.append(item)
