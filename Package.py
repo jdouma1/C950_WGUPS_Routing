9fc6abc5466b078eb006bf426d3177f9229695f0
Jacob Douma <doumajacob24@gmail.com>
2024-03-04 19:42:40 -0800

    Removes duplicate Graph and Vertex code in files. Optimizes imports in main.py.

--- Package.py
+++ Package.py
@@ -1,4 +1,7 @@
-#Class used for creating packages and package data for hash table
+import csv
+
+
+# Class used for creating packages and package data for hash table
 class Package:
     # Constructor for package to store address and other key information
     def __init__(self, packageId, address, city, state, zipCode, deliveryDeadline, edgeWeight):
@@ -13,3 +16,21 @@ class Package:
     # Method to overwrite how package is printed to print data instead of object
     def __str__(self):
         return "%s, %s, %s, %s, %s, %s, %s" % (self.packageId, self.address, self.city, self.state, self.zipCode, self.deliveryDeadline, self.edgeWeight)
+
+    # Method takes file and reads through entries to load packages into hash table
+def loadPackageData(fileName, packageHashTable):
+    with open(fileName) as packages:
+        packageData = csv.reader(packages, delimiter=',')
+        next(packageData)  # Skip the header
+        for package in packageData:
+            packageId = int(package[0])
+            address = package[1]
+            city = package[2]
+            state = package[3]
+            zipCode = int(package[4])
+            deliveryDeadline = package[5]
+            edgeWeight = int(package[6])
+
+            # Create and insert new package into hash table
+            newPackage = Package(packageId, address, city, state, zipCode, deliveryDeadline, edgeWeight)
+            packageHashTable.insert(packageId, newPackage)
--- main.py
+++ main.py
@@ -1,48 +1,12 @@
-import csv
+#import csv
 from HashTable import HashTable
-from Package import Package
+from Package import loadPackageData
+from Dijkstra import Vertex, Graph, dijkstraDeliveryRoute
 
 # Global variable for hash table
 packageHashTable = HashTable()
 
-
-# Dijkstra algorithm implementation for delivery route
-def dijkstraDeliveryRoute(graph, startVertex):
-    # Begin by putting all vertices in a queue of unvisited vertices
-    unvisitedQueue = []
-
-    for currVertex in graph.adjacencyList:
-        unvisitedQueue.append(currVertex)
-
-    # Start vertex has a distance of 0 from itself
-    startVertex.distance = 0
-
-    # Vertices are visited individually until the list is empty
-    while len(unvisitedQueue) > 0:
-        # Assume index 0 has the shortest distance
-        minDistanceIndex = 0
-
-        # Find if any shorter distance vertex exists and update the minimum
-        for i in range(1, len(unvisitedQueue)):
-            if unvisitedQueue[i].distance < unvisitedQueue[minDistanceIndex].distance:
-                minDistanceIndex = i
-
-        # Visit the vertex with the shortest distance
-        currVertex = unvisitedQueue.pop(minDistanceIndex)
-
-        # Check each path length from current vertex to all neighboring vertices
-        for adjacentVertex in graph.adjacencyList[currVertex]:
-            # Pull the edge weight from dictionary using key:value pair
-            #
-            edgeWeight = graph.edgeWeights[(currVertex, adjacentVertex)]
-            altPathDistance = currVertex.distance + edgeWeight
-
-            # Compare alternate path of currentVertex-->adjacentVertex to startVertex-->adjacentVertex
-            # If this alternate path is found to be shorter, update the adjacent vertex's distance and predecessor node
-            if altPathDistance < adjacentVertex.distance:
-                adjacentVertex.distance = altPathDistance
-                adjacentVertex.predecessor = currVertex
-
+'''
 # Method takes file and reads through entries to load packages into hash table
 def loadPackageData(fileName):
     with open(fileName) as packages:
@@ -60,11 +24,11 @@ def loadPackageData(fileName):
             # Create and insert new package into hash table
             newPackage = Package(packageId, address, city, state, zipCode, deliveryDeadline, edgeWeight)
             packageHashTable.insert(packageId, newPackage)
-
+'''
 
 if __name__ == '__main__':
     # Load packages into hash table from csv file
-    loadPackageData("Packages.csv")
+    loadPackageData("Packages.csv", packageHashTable)
 
     # Iterate over packages in hash table and print data
     for i in range(len(packageHashTable.table) + 1):
